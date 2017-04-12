e3214a28420bf8954c7208f0b5d6582f4bc43951
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-04-12 11:30:42 +0100

    Add dataset to app

--- dserve/__init__.py
+++ dserve/__init__.py
@@ -1,5 +1,6 @@
 """Script for running the dserve server."""
 
+import argparse
 import json
 
 from flask import (
@@ -7,11 +8,21 @@ from flask import (
     jsonify,
 )
 
+from dtoolcore import DataSet
+
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    return jsonify("")
+    return jsonify(app._dataset._admin_metadata)
+
+def main(dataset, port, debug):
+    app._dataset = dataset
+    app.run(port=port, debug=debug)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("dataset_path")
+    args = parser.parse_args()
+    dataset = DataSet.from_path(args.dataset_path)
+    main(dataset, 5000, True)
--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,11 +1,22 @@
 """Functional tests for dserve server."""
 
-import requests
+import os
 
-TEST_SERVER = "http://127.0.0.1:5000"
+import pytest
+import requests
+from flask import request
+
+HERE = os.path.dirname(__file__)
+SAMPLE_DATASET_PATH = os.path.join(HERE, "data", "cotyledon_images")
+
+PORT=5000
+TEST_SERVER = "http://127.0.0.1:{}".format(PORT)
 
 def test_root():
     url = TEST_SERVER
     r = requests.get(url)
     assert r.status_code == 200
     assert r.headers['content-type'].find("json") != -1
+
+    content = r.json()
+    assert "uuid" in content
