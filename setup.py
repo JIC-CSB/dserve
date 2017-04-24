a7807628007c0114680c48d7ddec72b54d0aaf4f
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-04-24 14:29:38 +0100

    Add version constraint on dtoolcore to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=['dserve'],
     include_package_data=True,
     install_requires=[
-        'dtoolcore',
+        'dtoolcore<=0.14.0',
         'flask',
         'flask_cors',
     ],
