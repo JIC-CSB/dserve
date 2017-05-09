800be06093ea24e93f25401616cd4e2a0a1e1f48
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-05-09 11:12:41 +0100

    Updated requitments in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=['dserve'],
     include_package_data=True,
     install_requires=[
-        'dtoolcore<=0.14.0',
+        'dtoolcore>=0.15.0',
         'flask',
         'flask_cors',
     ],
