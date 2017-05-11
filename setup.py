7e52aa37df857ebd1777049cb12890c5e6daae1d
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-05-11 14:43:26 +0100

    Update setup.py for release on pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from setuptools import setup
 
+version = "0.1.0"
+url = "https://github.com/JIC-CSB/dserve"
+readme = open('README.rst').read()
+
 setup(
     name='dserve',
     packages=['dserve'],
+    version=version,
+    description="Tool to serve a dataset over HTTP",
+    long_description=readme,
     include_package_data=True,
+    author="Tjelvar Olsson",
+    author_email="tjelvar.olsson@jic.ac.uk",
+    url=url,
+    download_url="{}/tarball/{}".format(url, version),
     install_requires=[
         'dtoolcore>=0.15.0',
         'flask',
@@ -12,4 +23,4 @@ setup(
     entry_points={
         'console_scripts': ['dserve=dserve.cli:main']
     },
-)
+    license="MIT")
